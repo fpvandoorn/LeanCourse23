31e8280e5839e58ca33f8776d778f3ad9f1cde3c
Floris van Doorn <fpvdoorn@gmail.com>
2023-10-12 23:31:51 +0200

    add assignment 1

---
+++ LeanCourse/Assignments/Assignment1.lean
@@ -0,0 +1,50 @@
+import LeanCourse.Common
+
+/-
+
+* Read chapter 1 and sections 2.1 and 2.2 in Mathematics in Lean.
+  https://leanprover-community.github.io/mathematics_in_lean
+
+* Do the following exercises (you don't have to hand these in):
+  - `LeanCourse/MIL/C02_Basics/S01_Calculating.lean`
+  - `LeanCourse/MIL/C02_Basics/S02_Proving_Identities_in_Algebraic_Structures.lean`
+  There are solutions in the solution folder in case you get stuck.
+
+* Hand in the solutions to the exercises below. Upload them on eCampus before
+-/
+
+/-
+Do the next exercise using only the `rw` tactic with lemmas `add_comm` and `add_assoc`.
+-/
+
+lemma exercise1_1 (a b c d : ℝ) : a + b + c + d = d + (b + a) + c := by sorry
+
+/-
+Also do the proof using `calc`, where on each `calc` line, you use `rw` only once.
+-/
+lemma exercise1_2 (a b c d : ℝ) : a + b + c + d = d + (b + a) + c := by sorry
+
+/-
+Do the following exercise using the `rw` tactic only.
+
+The following lemmas may be useful.
+
+`pow_two x       : x^2 = x*x`
+`mul_add a b c   : a*(b + c) = a*b + a*c`
+`add_mul a b c   : (a + b)*c = a*c + b*c`
+`mul_sub a b c   : a*(b - c) = a*b - a*c`
+`sub_mul a b c   : (a - b)*c = a*c - b*c`
+`add_sub a b c   : a + (b - c) = (a + b) - c`
+`sub_add a b c   : a - b + c = a - (b - c)`
+`add_assoc a b c : (a + b) + c = a + (b + c)`
+`sub_sub a b c   : a - b - c = a - (b + c)`
+`sub_self a      : a - a = 0`
+`add_zero a      : a + 0 = a`
+`zero_add a      : 0 + a = a`
+-/
+
+lemma exercise1_3 (a b : ℝ) : (a + b) * (a - b) = a^2 - b^2 := by sorry
+
+-- Now redo it with `ring`.
+
+lemma exercise1_4 (a b : ℝ) : (a + b) * (a - b) = a^2 - b^2 := by sorry
