6da593cd374f573d669c9c8d5fd32204197c765a
monnerjahnl0 <s6lumonn@uni-bonn.de>
2023-12-04 02:57:31 +0100

    Project Progress

--- LeanCourse/Project/constuctionlive.lean
+++ LeanCourse/Project/constuctionlive.lean
@@ -1,14 +1,99 @@
-import LeanCourse.Common
-import Mathlib.FieldTheory.IntermediateField
-import Mathlib.FieldTheory.Galois
-import Mathlib.FieldTheory.Adjoin
+import Mathlib
+
+-- Start LeanCourse.Common
+set_option warningAsError false
+
+section
+open Lean Parser Tactic
+
+macro (name := ring) "ring" : tactic =>
+  `(tactic| first | ring1 | ring_nf)
+
+macro (name := ring_at) "ring" cfg:config ? loc:location : tactic =>
+  `(tactic| ring_nf $cfg ? $loc)
+
+end
+
+namespace Nat
+open Lean Elab Term Meta
+
+def elabIdentFactorial : TermElab := fun stx expectedType? =>
+  match stx with
+  | `($name:ident) => do
+    let name := name.getId
+    if name.hasMacroScopes then
+      -- I think this would mean the name appears from within a quote.
+      -- I'm not sure how to properly deal with this, and it seems ok to just not.
+      throwUnsupportedSyntax
+    else
+      try
+        elabIdent stx expectedType?
+      catch e =>
+        match name with
+        | .str n s =>
+          if s.endsWith "!" then
+            let name' := Name.str n (s.dropRight 1)
+            try
+              elabTerm (← `(Nat.factorial $(mkIdent name'))) expectedType?
+            catch _ =>
+              throw e
+          else
+            throw e
+        | _ => throw e
+  | _ => throwUnsupportedSyntax
+
+attribute [scoped term_elab ident] elabIdentFactorial
+
+attribute [eliminator] Nat.recAux
+
+@[elab_as_elim]
+def two_step_induction {P : ℕ → Sort*} (zero : P 0) (one : P 1)
+    (step : ∀ (k : ℕ), (IH0 : P k) → (IH1 : P (k + 1)) → P (k + 2)) (n : ℕ) :
+    P n := by
+  induction n using Nat.strongRec with
+  | ind n ih =>
+    rcases n with _|n
+    · exact zero
+    rcases n with _|n
+    · exact one
+    apply step
+    · apply ih; linarith
+    · apply ih; linarith
+
+
+
+end Nat
+
+namespace Filter
+variable {α β : Type*} {m : α → β}
+@[gcongr]
+theorem map_le_map {F G : Filter α} (h : F ≤ G) : map m F ≤ map m G :=
+  map_mono h
+
+@[gcongr]
+theorem comap_le_comap {F G : Filter β} (h : F ≤ G) : comap m F ≤ comap m G :=
+  comap_mono h
+end Filter
+
+attribute [gcongr] interior_mono closure_mono
+
+section ExtraLemmas
+
+lemma pow_self_ne_zero (n : ℕ) : n ^ n ≠ 0 := by
+  by_cases hn : n = 0
+  · simp [hn]
+  · positivity
+
+open Real
+
+attribute [simp] div_left_inj' neg_eq_self_iff eq_neg_self_iff sqrt_eq_zero' Int.ModEq.rfl
+
+end ExtraLemmas
+-- END LeanCourse.Common
 
 set_option autoImplicit true
-
 open FiniteDimensional Polynomial
-
 open scoped Classical Polynomial
-
 
 namespace constructionProof
 
@@ -79,10 +164,11 @@ lemma third_root_of_two_not_in_M_inf (M := {⟨0,0⟩ ,⟨1,0⟩}) :
     apply minpoly.eq_of_irreducible_of_monic
     case hp1 => exact P_irreducible
     case hp2 => sorry
-    /- calc ↑(aeval (2 ^ (1 / 3))) (X ^ 3 - 2)
-      _ = (((2 : ℝ) ^ (1 / 3 : ℝ)) ^ 3) - 2 := by
-      _ = 2 - 2 := by simp
-      _ = 0 := by simp -/
+      /- simp
+      calc
+        _ = 2 ^ ((1/3) * 3) - 2 := by exact rfl
+        _ = 2 - 2 := by norm_num
+        _ = 0 := by simp -/ -- Worng 0 not of Type @OfNat.ofNat ℂ 0 Zero.toOfNat0 : ℂ
     case hp3 => rw[Polynomial.Monic.def]; apply monic_X_pow_sub_C; simp
   have p_deg : Polynomial.degree (P) = 3 := by
     calc degree P
