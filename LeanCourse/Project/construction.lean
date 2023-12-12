537e9cadaea2e362b18d3e5b92d7146a48ba6d20
monnerjahnl0 <s6lumonn@uni-bonn.de>
2023-12-12 13:14:55 +0100

    use monomial

--- LeanCourse/Project/construction.lean
+++ LeanCourse/Project/construction.lean
@@ -3,6 +3,7 @@ import Mathlib.FieldTheory.IntermediateField
 import Mathlib.FieldTheory.Galois
 import Mathlib.FieldTheory.Adjoin
 import Mathlib.RingTheory.Polynomial.RationalRoot
+import Mathlib
 
 set_option autoImplicit true
 
@@ -55,7 +56,7 @@ noncomputable def K_zero (M : Set Point) : IntermediateField ℚ  ℂ := Interme
   ∃ L : IntermediateField ℚ  ℂ ,  z ∈ L ↔
   \ -/
 
-lemma Classfication_z_in_M_inf (M : Set Point) (z : Point) :
+lemma Classfication_z_in_M_inf_2m (M : Set Point) (z : Point) :
   z ∈ M_inf M ↔ ∃ m : ℕ , ((2  : ℕ) ^ m : WithBot ℕ) = Polynomial.degree (minpoly (K_zero M) z.x)
    := sorry
 
@@ -69,13 +70,17 @@ lemma K_zero_eq_ration (M : Set Point) : M = {⟨0,0⟩ ,⟨1,0⟩} → (K_zero 
 section constructionDoubleCube
 
 noncomputable def P : (Polynomial ℚ) := X ^ 3 - 2 --TODO: Tidy up
+noncomputable def P' : (Polynomial ℚ) := monomial 3 1 - 2 --TODO: Tidy up
 
-lemma P_irreducible : Irreducible P := by sorry
+lemma P_irreducible : Irreducible P := by
+  rw[P]
+  by_contra h
+  sorry
 
 lemma third_root_of_two_not_in_M_inf (M := {⟨0,0⟩ ,⟨1,0⟩}) :
   (⟨(2 : ℝ) ^ (1/3), 0⟩ : Point) ∉ M_inf M := by
   by_contra h
-  rw[constructionProof.Classfication_z_in_M_inf M ⟨(2 : ℝ) ^ (1/3), 0⟩] at h
+  rw[constructionProof.Classfication_z_in_M_inf_2m M ⟨(2 : ℝ) ^ (1/3), 0⟩] at h
   obtain ⟨m, hm⟩ := h
 
   have p : minpoly ℚ (2  ^ (1/3): ℂ ) = X ^ 3 - 2 := by
@@ -83,8 +88,8 @@ lemma third_root_of_two_not_in_M_inf (M := {⟨0,0⟩ ,⟨1,0⟩}) :
     apply minpoly.eq_of_irreducible_of_monic
     case hp1 => exact P_irreducible
     case hp2 => sorry
-    /- calc ↑(aeval (2 ^ (1 / 3))) (X ^ 3 - 2)
-      _ = (((2 : ℝ) ^ (1 / 3 : ℝ)) ^ 3) - 2 := by
+    /- calc ↑(aeval (2 ^ (1 / 3))) (X ^ 3 - 2) --TODO: add simp_rpow https://github.com/teorth/symmetric_project/blob/master/SymmetricProject/Tactic/RPowSimp.lean#L305-L306
+      _ = (((2 : ℝ) ^ (1 / 3 : ℝ)) ^ 3) - 2 := by sorry
       _ = 2 - 2 := by simp
       _ = 0 := by simp -/
     case hp3 => rw[Polynomial.Monic.def]; apply monic_X_pow_sub_C; simp
@@ -105,24 +110,22 @@ lemma third_root_of_two_not_in_M_inf (M := {⟨0,0⟩ ,⟨1,0⟩}) :
 
 section constructionAngleTrisection
 
-noncomputable def H : Polynomial ℚ := 8 * X ^ 3 - 6 * X - 1
-noncomputable def H' : Polynomial ℚ := X ^ 3 - (6/8) * X - (1/8)
-noncomputable def H'' : Polynomial ℚ :=  1/8
+noncomputable def H : Polynomial ℚ := monomial 3 8 - monomial 1 6 - 1
+noncomputable def H' : Polynomial ℚ := monomial 3 1 - monomial 1 (6/8) - (1/8)
+noncomputable def H'' : Polynomial ℚ :=  monomial 0 (1/8)
 
 lemma H_irred : Irreducible H := by
   by_contra h
   simp only [H] at h
-  apply
   sorry
 
 lemma unit_of_H' : IsUnit H'' := by
   refine isUnit_of_dvd_one ?h
-  have h : (1 : ℚ[X]) = (8 : ℚ[X]) * ((1/8) : ℚ[X]) := by
-    ring_nf
+  have h : monomial 0 (8 * (1/8)) =  8  * H'' := by
+    rw[H'']
     symm
-    --rw[div_mul_cancel  (by norm_num)]
-    sorry
-    --apply div_mul_cancel
+    apply Polynomial.C_mul_monomial
+  simp at h
   exact Dvd.intro_left 8 (id h.symm)
 
 
